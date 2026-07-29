13058db3dca3b971e631de0023b5bd54cca135b8
Utsav <Utsav@GAZEBO2019.local>
2026-07-29 09:09:23 +0100

    udpate format

--- product/urls.py
+++ product/urls.py
@@ -20,22 +20,24 @@ from product.views.product_master_view import product_collection_api, product_de
 from product.views.technical_views import product_technical_api
 
 urlpatterns = [
+
+    # general related urls
     path('', product_collection_api, name='product-collection'),
     path('class/', product_class_list_api, name='product-class-list'),
     path('category/', product_category_list_api, name='product-category-list'),
     path('range/', product_range_list_api, name='product-range-list'),
     path('unit/', product_unit_list_api, name='product-unit-list'),
     path('<int:pk>/', product_detail_api, name='product-detail'),
+
+    # complaince related urls
     path('<int:pk>/technical/', product_technical_api, name='product-technical'),
     path('<int:pk>/allergens/', product_allergens_api, name='product-allergens'),
-    path(
-        '<int:pk>/allergens/<str:allergen_code>/',
-        product_allergen_detail_api,
-        name='product-allergen-detail',
-    ),
+    path( '<int:pk>/allergens/<str:allergen_code>/', product_allergen_detail_api, name='product-allergen-detail'),
     path('<int:pk>/nutrition/', product_nutrition_api, name='product-nutrition'),
     path('<int:pk>/ingredient-label/', product_ingredient_label_api, name='product-ingredient-label'),
     path('<int:pk>/acceptance/', product_acceptance_api, name='product-acceptance'),
+   
+    # product related urls
     path('<int:pk>/class/', product_class_api, name='product-class'),
     path('<int:pk>/range/', product_range_api, name='product-range'),
     path('<int:pk>/unit/', product_unit_api, name='product-unit'),
