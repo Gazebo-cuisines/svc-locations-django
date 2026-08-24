c017c88e25e373cec334a44e0eed9d146ed2dbf5
Utsav <Utsav@GAZEBO2019.local>
2026-08-24 09:27:38 +0100

    merge fix

---
+++ planning/migrations/0007_merge_excel_compare.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('planning', '0004_excel_compare_report_temp'),
+        ('planning', '0006_plan_number_and_name'),
+    ]
+
+    operations = []
